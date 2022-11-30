11cb14780db24070795f50a4afc4233864f46fc9
Nadire Yağmur Yılmaz <ndrylmz3306@gmail.com>
2022-11-30 23:21:24 +0300

    Add files via upload

---
+++ w1p1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("Workshop 1 Part-1\n\n");
+	printf("------------------\n\n\n");
+	printf("I'm displaying this use the 'printf' stdio\n");
+	printf("(standard input output) library function\n\n");
+	printf("Dear teacher, \n\n");
+	printf("\b I promise I will work hard from this day onward\n");
+	printf("\b I acknowlenge that pratice is extremely important\n");
+	printf("\b I will do all workshops, quizzes, and assignments.\n\n");
+	printf("Sincerely\n\n");
+	printf("Nadire Yagmur Yilmaz");
+	
+	
+
+	
+	return 0;
+}
