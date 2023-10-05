db13cf291d8400179c579b51202f9c01aed18cbe
Nadire Yağmur Yılmaz <ndrylmz3306@gmail.com>
2023-10-06 01:22:14 +0300

    Update w1p1.c

--- w1p1.c
+++ w1p1.c
@@ -1,22 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
-	printf("Workshop 1 Part-1\n\n");
-	printf("------------------\n\n\n");
-	printf("I'm displaying this use the 'printf' stdio\n");
-	printf("(standard input output) library function\n\n");
-	printf("Dear teacher, \n\n");
-	printf("\b I promise I will work hard from this day onward\n");
-	printf("\b I acknowlenge that pratice is extremely important\n");
-	printf("\b I will do all workshops, quizzes, and assignments.\n\n");
-	printf("Sincerely\n\n");
-	printf("Nadire Yagmur Yilmaz");
+#include<stdio.h> 
+void main()  
+{  
+    char first_name[] = "Nadire";
+    char last_name[] = "Yilmaz";
+    unsigned long int studentID = 21444651;
+    printf("Workshop 1 Part-1\n");
+    printf("=================\n\n");
+    printf("I'm displaying this using the 'printf' stdio\n");
+    printf("(standard input output) library function!\n\n");
+    printf("Dear teacher,\n\n");
+    printf("\tI promise I will work hard from this day onward.\n");
+    printf("\tI acknowledge that practice is extremely important, \n");
+    printf("\tso I will do all workshops, quizzes, and assignments.\n\n");
+    printf("Sincerely,\n\n");
+    printf("%s  %s \n", first_name,last_name);
+    printf("Student ID# %lu",studentID);   
+}  
 	
-	
 
 	
-	return 0;
-}
